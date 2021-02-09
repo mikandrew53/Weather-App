85c41ca7bdcb33974a89b04ed40e5383dac66b35
Andrew Mikaeel <andrew.mikaeel@ontariotechu.net>
2021-02-09 14:48:44 -0500

    proxy

--- weather.js
+++ weather.js
@@ -4,7 +4,8 @@ class Weather {
     }
     async getWeather (woeid) {
             this.woeid = woeid;
-            const weatherResponse = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
+            const weatherResponse = await fetch(`https://metaweatherproxy.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
+            // const weatherResponse = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
             // const weatherResponse = await fetch(`https://www.metaweather.com/api/location/${this.woeid}/`);
             if(weatherResponse.ok){
                 const weatherResponseData = await weatherResponse.json();
@@ -14,7 +15,7 @@ class Weather {
     }
 
     async searchCity(userCity){
-        const cityIdResponse = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${userCity}`);
+        const cityIdResponse = await fetch(`https://metaweatherproxy.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${userCity}`);
         // const cityIdResponse = await fetch(`https://www.metaweather.com/api/location/search/?query=${userCity}`);
         if (cityIdResponse.ok){
             const cityIdResponseData = await cityIdResponse.json();
@@ -42,7 +43,8 @@ class Weather {
         for(let i=0; i < 6; i++){
             let searchDate = date.slice(0,8)+day;
             // const dayResponse = await fetch(`https://www.metaweather.com/api/location/${this.woeid}/${searchDate}/`);
-            const dayResponse = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${this.woeid}/${searchDate}/`);
+            // const dayResponse = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${this.woeid}/${searchDate}/`);
+            const dayResponse = await fetch(`https://metaweatherproxy.herokuapp.com/https://www.metaweather.com/api/location/${this.woeid}/${searchDate}/`);
             if(dayResponse.ok){
                 const dayResponseData = await dayResponse.json();
                 weather.consolidated_weather.push(dayResponseData[0]);
